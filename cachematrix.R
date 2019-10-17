7d333302f2c0c2bb69534c734a623de8fe345119
jennypnguyen <55671856+jennypnguyen@users.noreply.github.com>
2019-10-16 22:17:06 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+##These functions will take a square matrix and find the inverse. If the inverse was previously solve, it will find the inverse from the cache and return that value.
 
-## Write a short comment describing this function
+##The function below will create a special "matrix" object that will cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) {  
+        i <- NULL
+        set <- function(y){
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get, 
+             setinverse = setinverse, 
+             getinverse = getinverse)
+        
 }
 
+##The function below computes the inverse of the special "matrix" returned by makeCacheMatrix from above. If the inverse was previously solved, then this cacheSolve function will retrieve this inverse value from the cache.
 
-## Write a short comment describing this function
+cacheSolve <- function(x, ...){  
+        i <- x$getinverse()
+        if(!is.null(i)){
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
+} 
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+
+#Testing
+
+x <- matrix(1:4, nrow = 2, ncol=2) #sample matrix
+#y <- matrix(c(-2, 1, 1.5, -0.5), nrow = 2, ncol = 2) #inverse of sample matrix
+#identical(solve(x), y) #Should be TRUE
+
+inverse_x <- makeCacheMatrix(x)
+cacheSolve(inverse_x)
